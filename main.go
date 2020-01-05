8ce1a2dadc4aac093b67bd01069b84fc083292a4
myoan <motoki.yoan@gmail.com>
2020-01-05 02:12:49 +0000

    Fix script

--- main.go
+++ main.go
@@ -3,11 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"fmt"
-	"os"
-
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
 )
 
 type User struct {
@@ -16,33 +11,7 @@ type User struct {
 }
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	username := os.Getenv("DB_USERNAME")
-	password := os.Getenv("DB_PASSWORD")
-	host     := os.Getenv("DB_HOST")
-	dbname   := os.Getenv("DB_DBNAME")
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", username, password, host, dbname))
-	log.Println("Connected to DB!!")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close() // 関数がリターンする直前に呼び出される
-
-	rows, err := db.Query("SELECT * FROM user") //
-	defer rows.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for rows.Next() {
-		var user User
-		err := rows.Scan(&user.ID, &user.Name)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-		w.Write([]byte(fmt.Sprintf("%d: %s\n", user.ID, user.Name)))
-	}
+	w.Write([]byte("hogehoge"))
 }
 
 func main() {
